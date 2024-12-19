b048fd610ffbed8322faa3ecd3f396547ef8a563
Elham <elhamwaheed777@gmail.com>
2024-12-19 18:46:32 +0500

    concept of object

---
+++ main.js
@@ -0,0 +1,10 @@
+// Obj
+const student = {
+  name: "Muhammad Elham Waheed",
+  age: 21,
+  cgpa: 3.2,
+  isPass: true,
+};
+student.age = 20;
+console.log(student["age"]); // cosnt obj ki key ko update kr saktey he
+
