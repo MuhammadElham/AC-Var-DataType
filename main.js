4775deacebb323bfca25ef51d5906f4d5bda465c
Elham <elhamwaheed777@gmail.com>
2024-12-19 19:02:37 +0500

    add profile question

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ const student = {
 student.age = 20;
 console.log(student["age"]); // cosnt obj ki key ko update kr saktey he
 
+const profile = {
+  name: "shardha",
+  isFollow: true,
+  message: true,
+  post: 195,
+  followers: 569000,
+  following: 4,
+  entrepreneur: true,
+  description: "To educate someone is highest privilege",
+};
+console.log(typeof(profile));
